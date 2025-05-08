843716aa03a9267bbdd14faf1dcfa91c5f022f64
Sz-Tim <tim.szewczyk@gmail.com>
2025-05-08 12:10:58 +0100

    cleaning up debugging scripts

--- code/4_fit_ensemble.R
+++ code/4_fit_ensemble.R
@@ -16,10 +16,10 @@ rstan_options(threads_per_chain = 1) # reduce_sum() or map_rect() in .stan
 source("code/fn/helpers.R")
 
 
-for(sim in 1:2) {
+for(sim in 1:10) {
 
   dat_dir <- glue("data/sim/sim_{str_pad(sim, 2, 'left', '0')}/")
-  stan_dat <- make_stan_data(dat_dir)
+  stan_dat <- make_stan_data(dat_dir, priors_only=F, force_detect_priors=T)
 
   stages <- c("Ch", "PA", "Ad")
   stage_trans <- c("Ch-PA", "PA-Ad")
@@ -55,206 +55,134 @@ for(sim in 1:2) {
     mutate(label=factor(label, levels=unique(label)))
 
 
-  # IEM pt 1: influx -> attachment ------------------------------------------
+  # IEM: full model ---------------------------------------------------------
 
-  out_pt1 <- stan(file="code/stan/IEM_pt1_ensAttach.stan", data=stan_dat$dat,
-                     chains=3, cores=3, iter=2000, refresh=100,
-                     init=0,
-                     pars=c("attach_beta", "nb_prec", "IP_bg", "IP_bg_m3", "ensWts_p"))
-  saveRDS(out_pt1, glue("{dat_dir}/out_pt1_stanfit.rds"))
+  out_full <- stan(file="code/stan/CopyOfIEM_full_FEMALE.stan",
+                   data=stan_dat$dat, chains=3, cores=3, iter=30, refresh=1,
+                   control=list(max_treedepth=20),
+                   init=0, seed=101,
+                   pars=c("attach_beta", "IP_bg", "IP_bg_m3", "ensWts_p",
+                          "surv_beta", "thresh_GDD", "lifespan",
+                          "detect_p", "nb_prec"))
+  saveRDS(out_full, glue("{dat_dir}/out_full_stanfit.rds"))
 
-  out_pt1 <- readRDS(glue("{dat_dir}/out_pt1_stanfit.rds"))
-  out_pt1_df <- as.data.frame(out_pt1) |>
+  out_full_df <- as.data.frame(out_full) |>
     select(-lp__) |>
     mutate(iter=row_number()) |>
     pivot_longer(-iter)
-  out_pt1_sum <- out_pt1_df |>
+  out_full_sum <- out_full_df |>
     group_by(name) |>
     sevcheck::get_intervals(value, type="qi")
-  write_csv(out_pt1_sum, glue("{dat_dir}posterior_summary_pt1.csv"))
-
-  dat_pt1_df <- tibble(
+  dat_full_df <- tibble(
     name=c(paste0("attach_beta[", 1:5, "]"),
-           "nb_prec", "IP_bg_m3",
+           "IP_bg", "IP_bg_m3",
            paste0("ensWts_p[", 1:stan_dat$dat$nSims, "]"),
-           "IP_bg"),
+           paste0("surv_beta[1,", 1:stan_dat$dat$nStages, "]"),
+           paste0("surv_beta[2,", 1:stan_dat$dat$nStages, "]"),
+           paste0("thresh_GDD[", 1:(stan_dat$dat$nStages-1), ",1]"),
+           paste0("thresh_GDD[", 1:(stan_dat$dat$nStages-1), ",2]"),
+           "lifespan",
+           paste0("detect_p[", 1:(stan_dat$dat$nStages), "]"), "nb_prec"),
     value=c(stan_dat$params$attach_beta,
-            stan_dat$params$nb_prec, stan_dat$params$IP_bg_m3,
+            stan_dat$params$IP_bg, stan_dat$params$IP_bg_m3,
             stan_dat$params$ensWts_p,
-            stan_dat$params$IP_bg)
+            t(stan_dat$params$surv_beta),
+            stan_dat$params$thresh_GDD,
+            stan_dat$params$lifespan,
+            stan_dat$params$detect_p,
+            stan_dat$params$nb_prec)
   )
 
-
-  attach_ls <- map(list(out_pt1_df, out_pt1_sum, dat_pt1_df),
+  attach_ls <- map(list(out_full_df, out_full_sum, dat_full_df),
                    ~.x |> filter(grepl("attach_beta", name)) |>
                      inner_join(param_key, by=join_by(name)))
   p_attach <- post_summary_plot(attach_ls, scales="free") +
     geom_vline(xintercept=0, linetype=3)
 
-  ensWts_ls <- map(list(out_pt1_df, out_pt1_sum, dat_pt1_df),
+  ensWts_ls <- map(list(out_full_df, out_full_sum, dat_full_df),
                    ~.x |> filter(grepl("ensWts", name)) |>
                      inner_join(param_key, by=join_by(name)))
   p_ensWts <- post_summary_plot(ensWts_ls, scales="free_y") +
     xlim(0, 1)
 
-  else_ls <- map(list(out_pt1_df, out_pt1_sum, dat_pt1_df),
-                 ~.x |> filter(!grepl("ensWts|attach_beta", name)) |>
-                   inner_join(param_key, by=join_by(name)))
-  p_else <- post_summary_plot(else_ls, ncol=3, scales="free")
-
-  plot_grid(p_attach, p_ensWts, p_else, nrow=3, align="hv", axis="trbl")
-  ggsave(glue("{dat_dir}/fig_pt1_pars.png"), width=10, height=6)
-
-
-
-
-  # IEM pt 2: attachment -> adults ------------------------------------------
-
-  out_pt2_pop <- stan(file="code/stan/IEM_pt2_onFish_pop.stan",
-                     data=stan_dat$dat, chains=3, cores=3, iter=1000, refresh=1,
-                     # control=list(max_treedepth=20),
-                     init=0, seed=10,
-                     pars=c("surv_beta", "pMoltF_beta", "pMoltM_beta", "nb_prec"))
-  saveRDS(out_pt2_pop, glue("{dat_dir}/out_pt2_pop_stanfit.rds"))
-
-
-  out_pt2_pop_df <- as.data.frame(out_pt2_pop) |>
-    select(-lp__) |>
-    mutate(iter=row_number()) |>
-    pivot_longer(-iter)
-  write_csv(out_pt2_pop_df, glue("{dat_dir}posterior_pt2_pop.csv"))
-  out_pt2_pop_sum <- out_pt2_pop_df |>
-    group_by(name) |>
-    sevcheck::get_intervals(value, type="qi")
-  write_csv(out_pt2_pop_sum, glue("{dat_dir}posterior_summary_pt2_pop.csv"))
-
-  dat_pt2_pop_df <- tibble(
-    name=c(paste0("surv_beta[1,", 1:4, "]"), paste0("surv_beta[2,", 1:4, "]"),
-           "nb_prec"),
-    value=c(t(stan_dat$params$surv_beta),
-            stan_dat$params$nb_prec)
-  )
-
-  surv_ls <- map(list(out_pt2_pop_df, out_pt2_pop_sum, dat_pt2_pop_df),
+  surv_ls <- map(list(out_full_df, out_full_sum, dat_full_df),
                  ~.x |> filter(grepl("surv_beta", name)) |>
                    inner_join(param_key, by=join_by(name)))
-  p_surv <- post_summary_plot(surv_ls, ncol=4, scales="free") +
+  p_surv <- post_summary_plot(surv_ls, ncol=3, scales="free") +
     geom_vline(xintercept=0, linetype=3)
 
-  pMoltInt_ls <- map(list(out_pt2_pop_df, out_pt2_pop_sum, dat_pt2_pop_df),
-                     ~.x |> filter(grepl("pMolt.*_beta\\[1", name)) |>
-                       inner_join(param_key, by=join_by(name)))
-  p_pMoltInt <- post_summary_plot(pMoltInt_ls, ncol=5, scales="free_y") +
-    geom_vline(xintercept=0, linetype=3)
+  pMolt_ls <- map(list(out_full_df, out_full_sum, dat_full_df),
+                  ~.x |> filter(grepl("thresh_GDD|lifespan", name)) |>
+                    inner_join(param_key, by=join_by(name)) |>
+                    filter(grepl("lifespan|moltF", label)))
+  p_pMolt <- post_summary_plot(pMolt_ls, ncol=4, scales="free_y")
 
-  pMoltTemp_ls <- map(list(out_pt2_pop_df, out_pt2_pop_sum, dat_pt2_pop_df),
-                      ~.x |> filter(grepl("pMolt.*_beta\\[2", name)) |>
-                        inner_join(param_key, by=join_by(name)))
-  p_pMoltTemp <- post_summary_plot(pMoltTemp_ls, ncol=5, scales="free_y") +
-    geom_vline(xintercept=0, linetype=3)
-
-  plot_grid(p_surv, p_pMoltInt, p_pMoltTemp, nrow=3, align="hv", axis="trbl",
-            rel_heights=c(1, 0.6, 0.6))
-
-  ggsave(glue("{dat_dir}/fig_pt2_pop_pars.png"), width=10, height=10)
-
-
-
-
-  # IEM pt 3: detection -----------------------------------------------------
-
-  out_pt3 <- stan(file="code/stan/IEM_pt3_detection.stan",
-                        data=stan_dat$dat, chains=3, cores=3, iter=3000, refresh=100,
-                        init=0)
-  saveRDS(out_pt3, glue("{dat_dir}/out_pt3_stanfit.rds"))
-
-  out_pt3 <- readRDS(glue("{dat_dir}/out_pt3_stanfit.rds"))
-  out_pt3_df <- as.data.frame(out_pt3) |>
-    select(-lp__) |>
-    mutate(iter=row_number()) |>
-    pivot_longer(-iter)
-  write_csv(out_pt3_df, glue("{dat_dir}posterior_pt3.csv"))
-
-  out_pt3_sum <- out_pt3_df |>
-    group_by(name) |>
-    sevcheck::get_intervals(value, type="qi")
-  write_csv(out_pt3_sum, glue("{dat_dir}posterior_summary_pt3.csv"))
-
-  dat_pt3_df <- tibble(
-    name=c(paste0("detect_p[", 1:stan_dat$dat$nStages, "]"), "nb_prec"),
-    value=c(stan_dat$params$detect_p,
-            stan_dat$params$nb_prec)
-  )
-
-
-  detectp_ls <- map(list(out_pt3_df, out_pt3_sum, dat_pt3_df),
+  detectp_ls <- map(list(out_full_df, out_full_sum, dat_full_df),
                     ~.x |> filter(grepl("detect_p", name)) |>
                       inner_join(param_key, by=join_by(name)))
   p_detectp <- post_summary_plot(detectp_ls, scales="free_y") +
     xlim(0, 1)
 
-  else_ls <- map(list(out_pt3_df, out_pt3_sum, dat_pt3_df),
-                 ~.x |> filter(!grepl("detect_p", name)) |>
+  else_ls <- map(list(out_full_df, out_full_sum, dat_full_df),
+                 ~.x |> filter(grepl("IP_bg|nb_prec", name)) |>
                    inner_join(param_key, by=join_by(name)))
-  p_else <- post_summary_plot(else_ls, ncol=1, scales="free")
+  p_else <- post_summary_plot(else_ls, ncol=3, scales="free")
 
-  plot_grid(p_detectp, p_else, nrow=1, align="hv", axis="trbl", rel_widths=c(1, 0.25))
-  ggsave(glue("{dat_dir}/fig_pt3_pars.png"), width=10, height=3)
+  plot_grid(p_ensWts, p_attach, p_surv, p_pMolt, p_detectp, p_else,
+            nrow=6, align="hv", axis="trbl", rel_heights=c(1, 1, 2, 1, 1, 1))
+  ggsave(glue("{dat_dir}/fig_full_pars.png"), width=10, height=3)
 
+  # IEM: full pop model -----------------------------------------------------
 
-
-
-  # IEM: full model ---------------------------------------------------------
-
-  out_full <- stan(file="code/stan/IEM_full_FEMALE.stan",
-                   data=stan_dat$dat, chains=1, cores=3, iter=1, refresh=1,
+  out_full <- stan(file="code/stan/IEM_pop5stage_mnDaysPrior.stan",
+                   data=stan_dat$dat, chains=3, cores=3, iter=40, refresh=1,
                    # control=list(max_treedepth=20),
                    init=0, seed=101,
-                   pars=c("attach_beta", "IP_bg", "IP_bg_m3", "ensWts_p",
-                          "surv_beta", "thresh_GDD", "lifespan",
+                   pars=c("IP_bg", "IP_bg_m3", "ensWts_p", "attach_beta",
+                          "surv_beta", "pMoltF_beta",
                           "detect_p", "nb_prec"))
   # saveRDS(out_full, glue("{dat_dir}/out_full_stanfit.rds"))
-  #
-  #
-  # out_full_df <- as.data.frame(out_full) |>
-  #   select(-lp__) |>
-  #   mutate(iter=row_number()) |>
-  #   pivot_longer(-iter)
-  # out_full_sum <- out_full_df |>
-  #   group_by(name) |>
-  #   sevcheck::get_intervals(value, type="qi")
-  # dat_full_df <- tibble(
-  #   name=c(paste0("attach_beta[", 1:5, "]"),
-  #          "IP_bg", "IP_bg_m3",
-  #          paste0("ensWts_p[", 1:2, "]"),
-  #          paste0("surv_beta[1,", 1:4, "]"), paste0("surv_beta[2,", 1:4, "]"),
-  #          paste0("thresh_GDD[", 1:3, ",1]"), paste0("thresh_GDD[", 1:3, ",2]"),
-  #          "lifespan",
-  #          paste0("detect_p[", 1:stan_dat$dat$nStages, "]"), "nb_prec"),
-  #   value=c(stan_dat$params$attach_beta,
-  #           stan_dat$params$IP_bg, stan_dat$params$IP_bg_m3,
-  #           stan_dat$params$ensWts_p,
-  #           t(stan_dat$params$surv_beta),
-  #           stan_dat$params$thresh_GDD,
-  #           stan_dat$params$lifespan,
-  #           stan_dat$params$detect_p,
-  #           stan_dat$params$nb_prec)
-  # )
-  #
-  # ggplot(out_full_df |> filter(name != "thresh_GDD[3,2]")) +
-  #   # stat_pointinterval(.width=c(0.5, 0.8, 0.95)) +
-  #   geom_density(aes(x=value)) +
-  #   geom_pointrange(data=out_full_sum |> filter(name != "thresh_GDD[3,2]"),
-  #                   aes(x=mn, xmin=L25, xmax=L75, y=0), linewidth=1.25) +
-  #   geom_linerange(data=out_full_sum |> filter(name != "thresh_GDD[3,2]"),
-  #                  aes(xmin=L10, xmax=L90, y=0), linewidth=0.8) +
-  #   geom_linerange(data=out_full_sum |> filter(name != "thresh_GDD[3,2]"),
-  #                  aes(xmin=L025, xmax=L975, y=0), linewidth=0.5) +
-  #   ylim(0, NA) +
-  #   geom_point(data=dat_full_df |> filter(name != "thresh_GDD[3,2]"),
-  #              aes(x=value, y=0), colour="red", shape=1) +
-  #   facet_wrap(~name, scales="free", ncol=4)
-  # ggsave(glue("{dat_dir}/fig_full_pars.png"), width=10, height=3)
+
+
+  out_full_df <- as.data.frame(out_full) |>
+    select(-lp__) |>
+    mutate(iter=row_number()) |>
+    pivot_longer(-iter)
+  out_full_sum <- out_full_df |>
+    group_by(name) |>
+    sevcheck::get_intervals(value, type="qi")
+  dat_full_df <- tibble(
+    name=c(paste0("attach_beta[", 1:5, "]"),
+           "IP_bg", "IP_bg_m3",
+           paste0("ensWts_p[", 1:6, "]"),
+           paste0("surv_beta[1,", 1:3, "]"), paste0("surv_beta[2,", 1:3, "]"),
+           paste0("thresh_GDD[", 1:2, ",1]"), paste0("thresh_GDD[", 1:2, ",2]"),
+           "lifespan",
+           paste0("detect_p[", 1:stan_dat$dat$nStages, "]"), "nb_prec"),
+    value=c(stan_dat$params$attach_beta,
+            stan_dat$params$IP_bg, stan_dat$params$IP_bg_m3,
+            stan_dat$params$ensWts_p,
+            t(stan_dat$params$surv_beta),
+            stan_dat$params$thresh_GDD,
+            stan_dat$params$lifespan,
+            stan_dat$params$detect_p,
+            stan_dat$params$nb_prec)
+  )
+
+  ggplot(out_full_df) +
+    # stat_pointinterval(.width=c(0.5, 0.8, 0.95)) +
+    geom_density(aes(x=value)) +
+    geom_pointrange(data=out_full_sum,
+                    aes(x=mn, xmin=L25, xmax=L75, y=0), linewidth=1.25) +
+    geom_linerange(data=out_full_sum,
+                   aes(xmin=L10, xmax=L90, y=0), linewidth=0.8) +
+    geom_linerange(data=out_full_sum,
+                   aes(xmin=L025, xmax=L975, y=0), linewidth=0.5) +
+    ylim(0, NA) +
+    geom_point(data=dat_full_df,
+               aes(x=value, y=0), colour="red", shape=1) +
+    facet_wrap(~name, scales="free", ncol=4)
+  ggsave(glue("{dat_dir}/fig_full_pars.png"), width=10, height=3)
 
 }
 
