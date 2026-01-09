a0b524028cc7776071b417b66a78a7ecd3e0439b
Sz-Tim <tim.szewczyk@gmail.com>
2026-01-09 09:32:02 +0000

    new biotracker version updates

--- code/2_run_biotracker.R
+++ code/2_run_biotracker.R
@@ -10,6 +10,7 @@ library(sevcheck) # devtools::install_github("Sz-Tim/sevcheck")
 library(biotrackR) # devtools::install_github("Sz-Tim/biotrackR")
 library(doFuture)
 library(sf)
+dirf("code/fn", ".R") |> walk(source)
 theme_set(theme_bw() + theme(panel.grid=element_blank()))
 
 set.seed(1001)
@@ -17,8 +18,8 @@ set.seed(1001)
 
 # define parameters -------------------------------------------------------
 
-cores_per_sim <- 20
-parallel_sims <- 4
+cores_per_sim <- 15
+parallel_sims <- 2
 start_date <- "2023-01-01"
 end_date <- "2024-12-31"
 nDays <- length(seq(ymd(start_date), ymd(end_date), by=1))
@@ -31,55 +32,36 @@ dirs <- switch(
              hf0="/home/sa04ts/hydro/etive28/Archive",
              hf1="/home/sa04ts/hydro/WeStCOMS2/Archive",
              jdk="/home/sa04ts/.jdks/jdk-23.0.1/bin/java",
-             jar="/home/sa04ts/biotracker/biotracker_v2-0-0.jar",
+             jar="/home/sa04ts/biotracker/biotracker_v2-2-1.jar",
              out=glue("{getwd()}/out/biotracker")),
   windows=list(proj=getwd(),
                mesh="E:/hydro",
                hf0="E:/hydro/etive28/Archive",
                hf1="E:/hydro/WeStCOMS2/Archive",
                jdk="C:/Users/sa04ts/.jdks/openjdk-23.0.2/bin/javaw",
-               jar="C:/Users/sa04ts/OneDrive - SAMS/Projects/03_packages/biotracker/out/biotracker.jar",
-               out=glue("D:/EtiveLice/out/biotracker"))
+               jar="C:/Users/sa04ts/OneDrive - SAMS/Projects/03_packages/biotracker/out/biotracker_v2-2-1.jar",
+               out=glue("E:/EtiveLice/out/biotracker"))
 )
 
+n_sim <- 20
 post_dir <- glue("{dirs$proj}/data/lit/fit")
-egg_post <- list(constant=tibble(b=rnorm(4000, 28.2, 2)),
+egg_post <- list(constant=read_csv(glue("{post_dir}/egg_temp_constant_post.csv"), show_col_type=F),
                  logistic=read_csv(glue("{post_dir}/egg_temp_logistic_post.csv"), show_col_types=F),
                  linear=read_csv(glue("{post_dir}/egg_temp_linear_post.csv"), show_col_types=F)) |>
   map(~.x |> mutate(across(everything(), ~signif(.x, 3))) |>
         unite("all", everything(), sep=",") %>%
         .$all)
-mort_post <- list(constant=tibble(b=plogis(rnorm(4000, qlogis(0.01), 0.25))),
-                 logistic=read_csv(glue("{post_dir}/mort_sal_logistic_post.csv"), show_col_types=F)) |>
+mort_post <- list(constant=read_csv(glue("{post_dir}/mort_sal_constant_post.csv"), show_col_types=F),
+                  logistic=read_csv(glue("{post_dir}/mort_sal_logistic_post.csv"), show_col_types=F)) |>
   map(~.x |> mutate(across(everything(), ~signif(.x, 3))) |>
         unite("all", everything(), sep=",") %>%
         .$all)
-n_sim <- 20
-swim_mx <- MASS::mvrnorm(n_sim, c(0,0), matrix(c(1, 0.6, 0.6, 1), nrow=2))
-light_mx <- MASS::mvrnorm(n_sim, c(0,0), matrix(c(1, 0.6, 0.6, 1), nrow=2))
-salMin_mx <- MASS::mvrnorm(n_sim, c(0,0), matrix(c(1, 0.6, 0.6, 1), nrow=2))
-salMax_mx <- MASS::mvrnorm(n_sim, c(0,0), matrix(c(1, 0.6, 0.6, 1), nrow=2))
-sim.i <- tibble(D_h=exp(runif(n_sim, log(1e-4), log(10))),
-                D_hVert=exp(runif(n_sim, log(1e-6), log(1))),
-                mortSal_fn=sample(c("constant", "logistic"), n_sim, replace=T),
-                eggTemp_fn=sample(c("constant", "logistic"), n_sim, replace=T),
-                lightThreshCopepodid=qunif(pnorm(light_mx[,1]), (2e-6)^0.5, (2e-4)^0.5)^2,
-                lightThreshNauplius=qunif(pnorm(light_mx[,2]), (0.05)^0.5, (0.5)^0.5)^2,
-                swimUpSpeedMean=-1/qunif(pnorm(swim_mx[,1]), 30, 1800), # 00:00:30 - 00:30:00 to swim 1 m
-                swimDownSpeedMean=1/qunif(pnorm(swim_mx[,2]), 30, 1800), # 00:00:30 - 00:30:00 to swim 1 m
-                passiveSinkRateSal=sample(c(T, F), n_sim, replace=T),
-                salinityThreshCopepodidMin=qunif(pnorm(salMin_mx[,1]), 20, 28),
-                salinityThreshNaupliusMin=qunif(pnorm(salMin_mx[,2]), 20, 28),
-                salinityThreshCopepodidMax=pmin(salinityThreshCopepodidMin + qunif(pnorm(salMax_mx[,1]), 0.1, 6), 32),
-                salinityThreshNaupliusMax=pmin(salinityThreshNaupliusMin + qunif(pnorm(salMax_mx[,2]), 0.1, 6), 32),
-                viableDegreeDays=runif(n_sim, 35, 45)) |>
-  mutate(across(where(is.numeric), ~signif(.x, 5))) |>
-  mutate(i=str_pad(row_number(), 2, "left", "0"),
-         outDir=glue("{dirs$out}/sim_{i}/")) |>
-  rowwise() |>
-  mutate(eggTemp_b=sample(egg_post[[eggTemp_fn]], 1),
-         mortSal_b=sample(mort_post[[mortSal_fn]], 1)) |>
-  ungroup()
+sink_post <- read_csv(glue("{post_dir}/sink_sal_linear_post.csv"), show_col_types=F) |>
+  mutate(across(everything(), ~.x/1e3)) #mm/s to m/s
+sim.i <- sample_parameter_distributions(n_sim, dirs$out, "lhs", egg_post, mort_post, sink_post) |>
+  mutate(connectivityThresh=30,
+         variableDh="false",
+         variableDhV="false")
 write_csv(sim.i, glue("{dirs$out}/sim_i.csv"))
 sim_seq <- 1:nrow(sim.i)
 
@@ -111,20 +93,23 @@ walk(sim_seq,
        hfDirPrefix1="netcdf_",
        hfFilePrefix1="westcoms2",
        # sites
-       sitefile=glue("{dirs$proj}/data/pen_sites_linnhe_2023-2024.csv"),
-       sitefileEnd=glue("{dirs$proj}/data/pen_sites_etive_2023-2024.csv"),
-       siteDensityPath=glue("{dirs$proj}/data/lice_daily_2023-01-01_2024-12-31.csv"),
+       sitefile=glue("E:/EtiveLice/data/pen_sites_linnhe_2023-2024.csv"),
+       sitefileEnd=glue("E:/EtiveLice/data/pen_sites_linnhe_2023-2024.csv"),
+       siteDensityPath=glue("E:/EtiveLice/data/lice_daily_2023-01-01_2024-12-31_05lpf_maxB.csv"),
        # sitefile="D:/EtiveLice/data/pen_sites_linnhe_2023-2024.csv",
        # sitefileEnd="D:/EtiveLice/data/pen_sites_etive_2023-2024.csv",
        # siteDensityPath="D:/EtiveLice/data/lice_daily_2023-01-01_2024-12-31.csv",
        # dynamics
-       D_hVert=sim.i$D_hVert[.x],
+       variableDh=sim.i$variableDh[.x],
+       variableDhV=sim.i$variableDhV[.x],
        D_h=sim.i$D_h[.x],
+       D_hVert=sim.i$D_hVert[.x],
        stepsPerStep=30,
        stokesDrift="false",
        # biology
        fixDepth="false",
-       startDepth=10,
+       startDepth=3,
+       maxDepth=sim.i$maxDepth[.x],
        eggTemp_fn=sim.i$eggTemp_fn[.x],
        eggTemp_b=sim.i$eggTemp_b[.x],
        mortSal_fn=sim.i$mortSal_fn[.x],
@@ -135,26 +120,30 @@ walk(sim_seq,
        salinityThreshNaupliusMax=sim.i$salinityThreshNaupliusMax[.x],
        lightThreshCopepodid=sim.i$lightThreshCopepodid[.x],
        lightThreshNauplius=sim.i$lightThreshNauplius[.x],
-       swimUpSpeedCopepodidMean=sim.i$swimUpSpeedMean[.x],
-       swimUpSpeedCopepodidStd=abs(sim.i$swimUpSpeedMean[.x]/5),
-       swimDownSpeedCopepodidMean=sim.i$swimDownSpeedMean[.x],
-       swimDownSpeedCopepodidStd=sim.i$swimDownSpeedMean[.x]/5,
-       swimUpSpeedNaupliusMean=sim.i$swimUpSpeedMean[.x]/2,
-       swimUpSpeedNaupliusStd=abs(sim.i$swimUpSpeedMean[.x]/10),
-       swimDownSpeedNaupliusMean=sim.i$swimDownSpeedMean[.x]/2,
-       swimDownSpeedNaupliusStd=sim.i$swimDownSpeedMean[.x]/10,
-       passiveSinkingIntercept=if_else(sim.i$passiveSinkRateSal[.x], 0.001527, sim.i$swimDownSpeedMean[.x]),
-       passiveSinkingSlope=if_else(sim.i$passiveSinkRateSal[.x], -1.68e-5, 0),
+       swimColdNauplius=sim.i$swimColdNauplius[.x],
+       swimUpSpeedCopepodidMean=sim.i$swimUpSpeedCopepodidMean[.x],
+       swimUpSpeedCopepodidStd=abs(sim.i$swimUpSpeedCopepodidMean[.x]/5),
+       swimDownSpeedCopepodidMean=sim.i$swimDownSpeedCopepodidMean[.x],
+       swimDownSpeedCopepodidStd=sim.i$swimDownSpeedCopepodidMean[.x]/5,
+       swimUpSpeedNaupliusMean=sim.i$swimUpSpeedNaupliusMean[.x],
+       swimUpSpeedNaupliusStd=abs(sim.i$swimUpSpeedNaupliusMean[.x]/5),
+       swimDownSpeedNaupliusMean=sim.i$swimDownSpeedNaupliusMean[.x],
+       swimDownSpeedNaupliusStd=sim.i$swimDownSpeedNaupliusMean[.x]/5,
+       passiveSinkingIntercept=sim.i$passiveSinkInt[.x],
+       passiveSinkingSlope=sim.i$passiveSinkSlope[.x],
        viableDegreeDays=sim.i$viableDegreeDays[.x],
        connectivityThresh=30,
        # recording
        verboseSetUp="true",
        recordConnectivity="true",
-       connectivityInterval=24,
+       connectivityInterval=1,
        connectDepth1_min=0,
        connectDepth1_max=20,
        recordPsteps= "false",
        recordVertDistr="false",
+       recordSiteEnv="true",
+       siteEnvInterval=1,
+       siteEnvMaxDepth=20,
        recordElemActivity="false"))
 
 
@@ -184,6 +173,8 @@ pens_linnhe <- read_csv("data/pen_sites_linnhe_2023-2024.csv")
 pens_etive <- read_csv("data/pen_sites_etive_2023-2024.csv")
 etive_farms <- read_csv("data/pen_sites_etive_2023-2024.csv") |>
   mutate(sepaSite=str_split_fixed(pen, "_", 2)[,1])
+linnhe_farms <- read_csv("data/pen_sites_linnhe_2023-2024.csv") |>
+  mutate(sepaSite=str_split_fixed(pen, "_", 2)[,1])
 
 farm_dat <- read_csv("data/lice_biomass_2017-01-01_2024-12-31.csv") |>
   filter(between(date, ymd(start_date), ymd(end_date))) |>
@@ -191,27 +182,81 @@ farm_dat <- read_csv("data/lice_biomass_2017-01-01_2024-12-31.csv") |>
 write_csv(farm_dat, "data/sim/inputs/farm_dat.csv")
 
 # Site conditions
-site_env_df <- dirf(glue("{dirs$out}/sim_01"), "siteConditions") |>
-  map_dfr(~read_csv(.x, show_col_types=F,
-                    col_select=c(site, depth, mesh, u, v, w, uv, salinity, temperature)) |>
-            mutate(date=ymd(str_split_fixed(basename(.x), "_", 3)[,2]))) |>
+# TODO: Add depth from siteEnv_Average.csv
+site_env_df <- dirf(glue("{dirs$out}/sim_01"), "siteEnv_2") |>
+  map_dfr(~read_csv(.x, show_col_types=F) |>
+            mutate(elapsedHours=str_sub(str_split_fixed(basename(.x), "_", 3)[,3], 1, -5))) |>
   rename(pen=site) |>
-  select(-mesh) |>
   mutate(pen=paste0(str_split_fixed(pen, "_", 2)[,1],
                     "_",
-                    str_pad(str_split_fixed(pen, "_", 2)[,2], 2, "left", "0"))) |>
-  inner_join(etive_farms |> select(sepaSite, pen)) |>
-  select(sepaSite, pen, date, depth, u, v, w, uv, salinity, temperature)
+                    str_pad(str_split_fixed(pen, "_", 2)[,2], 2, "left", "0")),
+         time=ymd(start_date) + dhours(as.numeric(elapsedHours))) |>
+  inner_join(linnhe_farms |> select(sepaSite, pen)) #|>
+  # select(sepaSite, pen, date, depth, u, v, w, uv, salinity, temperature)
 write_csv(site_env_df, "data/sim/inputs/farm_env.csv")
+
+
+site_env_df |>
+  select(sepaSite, time, pen, uv, salinity, temperature, light) |>
+  pivot_longer(4:7) |>
+  group_by(name) |>
+  mutate(valRel=(value-min(value))/(max(value)-min(value)),
+         value=c(scale(value))) |>
+  ungroup() |>
+  mutate(pen=factor(pen,
+                    levels=c(t(outer(paste0(c("FFMC27", "SAR1", "APT1", "FFMC32", "FFMC84"), "_"),
+                                     str_pad(1:20, 2, "left", "0"),
+                                     paste0)))),
+         sepaSite=factor(sepaSite, levels=c("FFMC27", "SAR1", "APT1", "FFMC32", "FFMC84"))) |>
+  ggplot(aes(time, pen, fill=valRel)) +
+  geom_raster() +
+  facet_grid(name~., axes="all_x", axis.labels="margins") +
+  scale_fill_viridis_c(option="turbo") +
+  scale_x_datetime(date_breaks="2 days", date_labels="%b-%d", date_minor_breaks="1 day") +
+  theme(axis.text.y=element_text(size=6))
+
+site_env_df |>
+  select(sepaSite, time, pen, uv, salinity, temperature, light) |>
+  pivot_longer(4:7) |>
+  group_by(name, sepaSite, time) |>
+  summarise(value=mean(value)) |>
+  group_by(name) |>
+  mutate(valRel=(value-min(value))/(max(value)-min(value)),
+         value=c(scale(value))) |>
+  ungroup() |>
+  mutate(sepaSite=factor(sepaSite, levels=c("FFMC27", "SAR1", "APT1", "FFMC32", "FFMC84"))) |>
+  ggplot(aes(time, sepaSite, fill=valRel)) +
+  geom_raster() +
+  facet_grid(name~., axes="all_x", axis.labels="margins") +
+  scale_fill_viridis_c(option="turbo") +
+  scale_x_datetime(date_breaks="2 days", date_labels="%b-%d", date_minor_breaks="1 day")
+
+site_env_df |>
+  select(sepaSite, time, pen, uv, salinity, temperature, light) |>
+  pivot_longer(4:7) |>
+  group_by(name) |>
+  ungroup() |>
+  mutate(pen=factor(pen,
+                    levels=c(t(outer(paste0(c("FFMC27", "SAR1", "APT1", "FFMC32", "FFMC84"), "_"),
+                                     str_pad(1:20, 2, "left", "0"),
+                                     paste0)))),
+         sepaSite=factor(sepaSite, levels=c("FFMC27", "SAR1", "APT1", "FFMC32", "FFMC84"))) |>
+  ggplot(aes(time, value, group=pen, colour=sepaSite)) +
+  geom_line() +
+  scico::scale_colour_scico_d(palette="devon", end=0.8, direction=1, guide="none") +
+  facet_grid(name~., axes="all_x", axis.labels="margins", scales="free_y") +
+  scale_x_datetime(date_breaks="2 days", date_labels="%b-%d", date_minor_breaks="1 day")
 
 # Influx
 mesh_fp <- st_read(glue("{dirs$mesh}/etive28_mesh_footprint.gpkg"))
 c_long <- dirrf(dirs$out, "connectivity.*csv") |>
   map_dfr(~load_connectivity(.x,
                              source_names=pens_linnhe$pen,
-                             dest_names=pens_etive$pen,
+                             dest_names=pens_linnhe$pen,
                              liceScale=1) |>
-            mutate(sim=str_sub(str_split_fixed(.x, "sim_", 2)[,2], 1, 2)))
+            mutate(sim=str_sub(str_split_fixed(.x, "sim_", 2)[,2], 1, 2),
+                   elapsedHours=str_sub(str_split_fixed(basename(.x), "_", 4)[,4], 1, -5))) |>
+  mutate(time=ymd(start_date) + dhours(as.numeric(elapsedHours)))
 site_areas <- sim.i |>
   mutate(connectivityThresh=30) |>
   select(i, connectivityThresh) |>
@@ -248,3 +293,57 @@ c_daily <- list(
   mutate(sepaSite=str_split_fixed(pen, "_", 2)[,1])
 
 write_csv(c_daily, "data/sim/inputs/influx.csv")
+
+
+
+c_long |>
+  group_by(destination, time, sim) |>
+  summarise(value=sum(value)) |>
+  ungroup() |>
+  mutate(sepaSite=str_split_fixed(destination, "_", 2)[,1]) |>
+  filter(sepaSite=="SAR1") |>
+  inner_join(site_env_df, by=join_by(sepaSite==sepaSite, time==time, destination==pen)) |>
+  ggplot(aes(time)) +
+  geom_rug(aes(colour=uv), linewidth=1.5, length=unit(1, "npc")) +
+  geom_point(aes(y=value, group=destination), shape=1) +
+  geom_line(aes(y=value, group=destination), linewidth=1) +
+  scale_colour_viridis_c(option="turbo") +
+  facet_grid(destination~.)
+
+c_long |>
+  group_by(destination, time, sim) |>
+  summarise(value=sum(value)) |>
+  ungroup() |>
+  mutate(sepaSite=str_split_fixed(destination, "_", 2)[,1]) |>
+  group_by(sepaSite, time, sim) |>
+  summarise(lice=mean(value)) |>
+  ungroup() |>
+  inner_join(site_env_df |>
+               group_by(sepaSite, time) |>
+               summarise(across(where(is.numeric), mean)) |>
+               ungroup(),
+             by=join_by(sepaSite==sepaSite, time==time)) |>
+  ggplot(aes(time)) +
+  geom_rug(aes(colour=uv), linewidth=1.5, length=unit(1, "npc")) +
+  geom_point(aes(y=lice), shape=1) +
+  geom_line(aes(y=lice), linewidth=1) +
+  scale_colour_viridis_c(option="turbo") +
+  facet_wrap(~sepaSite, scales="free_y")
+
+
+c_long |>
+  group_by(destination, time, sim) |>
+  summarise(value=sum(value)) |>
+  ungroup() |>
+  mutate(sepaSite=str_split_fixed(destination, "_", 2)[,1]) |>
+  group_by(sepaSite, time, sim) |>
+  summarise(lice=mean(value)) |>
+  ungroup() |>
+  inner_join(site_env_df |>
+               group_by(sepaSite, time) |>
+               summarise(across(where(is.numeric), mean)) |>
+               ungroup(),
+             by=join_by(sepaSite==sepaSite, time==time)) |>
+  ggplot(aes(salinity, lice)) +
+  geom_point(shape=1, alpha=0.5) +
+  facet_wrap(~sepaSite, scales="free_y")
