707fcc709114141a27acccfc61af783a8a6c065b
Sz-Tim <tim.szewczyk@gmail.com>
2026-03-12 11:36:49 +0000

    running with smaller ensemble

--- code/3_simulate_verification_data.R
+++ code/3_simulate_verification_data.R
@@ -8,8 +8,7 @@ library(tidyverse)
 library(glue)
 dir("code/fn", ".R$", full.names=T) |> walk(source)
 
-n_sims <- 20
-
+n_sims <- 50
 
 for(i in 1:n_sims) {
   sim_number <- str_pad(length(dir("data/sim", "sim"))+1, 2, 'left', '0')
